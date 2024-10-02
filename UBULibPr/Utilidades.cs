135a69624136573b8d32df8bbc630983f471e3c1
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-10-02 19:00:37 +0200

    Agregar archivos de proyecto.

---
+++ UBULibPr/Utilidades.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UBULibPr
+{
+    public class Utilidades
+    {
+        public string Encriptar(string cadena)
+        {
+            string result = string.Empty;
+            byte[] encryted = System.Text.Encoding.UTF8.GetBytes(cadena);
+            SHA256 mySHA256 = SHA256Managed.Create();
+            encryted = mySHA256.ComputeHash(encryted);
+            result = System.Text.Encoding.ASCII.GetString(encryted);
+            return result;
+        }
+        public int CompruebaContrasena(string cadena)
+        {
+            string caracteresPermitdos = "abcdefghijklmnopqrstuvwxyz";
+            string numerosPermitidos = "0123456789";
+            string caracteresEspeciales = "!@#$%^&*()_+?¿";
+            int puntuacion = 0;
+
+            // Iniciamos los compronbantes
+            if (cadena.Contains(" ")) return 0;
+            if (cadena.Length > 8) puntuacion++;
+            if (cadena.Intersect(caracteresPermitdos).Count() > 0) puntuacion++;
+            if (cadena.Intersect(caracteresPermitdos.ToUpper()).Count() > 0) puntuacion++;
+            if (cadena.Intersect(numerosPermitidos).Count() > 0) puntuacion++;
+            if (cadena.Intersect(caracteresEspeciales).Count() > 0) puntuacion++;
+
+            return puntuacion;
+        }
+
+
+    }
+}
---
+++ UBULibPrTests/UtilidadesTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UBULibPr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UBULibPr.Tests
+{
+    [TestClass()]
+    public class UtilidadesTests
+    {
+        [TestMethod()]
+        public void EncriptarTest()
+        {
+            Utilidades utilidades = new Utilidades();
+            string cadena1 = "HolaContraseña";
+            string cadena2 = "holacontraseña";
+            string cadena3 = "HolaContraseña";
+            string cadena4 = "";
+            Assert.AreEqual(utilidades.Encriptar(cadena1), utilidades.Encriptar(cadena3));
+            Assert.AreNotEqual(utilidades.Encriptar(cadena1), utilidades.Encriptar(cadena2));
+            Assert.AreNotEqual(utilidades.Encriptar(cadena1), utilidades.Encriptar(cadena4));
+            Assert.AreNotEqual(utilidades.Encriptar(cadena2), utilidades.Encriptar(cadena4));
+        }
+
+        [TestMethod()]
+        /*[DataRow("a", 1)]
+         * [DataRow("2", 2)]
+         * [DataRow("A", 3)]
+         * public void CompruebaContrasenaTest(string cadena, int resultado)
+         *      Utilidades utilidades = new Utilidades();
+         *      Assert.AreEqual(utilidades.CompruebaContrasena(cadena), resultado);
+         *      o tambien se puede hacer de la siguiente forma
+         *      Assert.IsTrue(utilidades.CompruebaContrasena(cadena) == resultado);
+         */
+        public void CompruebaContrasenaTest()
+        {
+            Utilidades utilidades = new Utilidades();
+            string cadena1 = "sololetrasminusculas";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena1), 2);
+
+            string cadena2 = "SOLOLETRASMAYUSCULAS";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena2), 2);
+
+            string cadena3 = "SoloLetrasMinusculasYMayusculas";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena3), 3);
+
+            string cadena4 = "01";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena4), 1);
+
+            string cadena5 = "SoloLetrasMinusculasYMayusculas1234567890";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena5), 4);
+
+            string cadena6 = "";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena6), 0);
+
+            string cadena7 = "SoloLetrasMinusculasYMayusculas1234567890!";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena7), 5);
+
+            string cadena8 = "@";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena8), 1);
+
+            string cadena9 = "           ";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena9), 0);
+
+            string cadena10 = "Esto es una contraseña de 8 caracteres ?";
+            Assert.AreEqual(utilidades.CompruebaContrasena(cadena10), 0);
+        }
+    }
+}
