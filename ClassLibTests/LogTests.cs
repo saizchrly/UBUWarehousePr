54dfa8e0b58a001dc8f83591cc8201f60348d137
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-10-03 14:55:49 +0200

    hacemos los test de caja blanca del proyecto ClassLib

---
+++ ClassLibTests/LogTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ClassLib.Tests
+{
+    [TestClass]
+    public class LogTests
+    {
+        [TestMethod]
+        public void EscribirLog_CreaArchivoSiNoExiste()
+        {
+            string usuario = "testuser";
+            string accion = "Test action";
+            string rutaArchivo = usuario + ".log";
+
+            Log.escribirLog(usuario, accion);
+
+            Assert.IsTrue(File.Exists(rutaArchivo));
+
+            File.Delete(rutaArchivo);
+        }
+
+        [TestMethod]
+        public void EscribirLog_AgregaTextoConFechaYAccion()
+        {
+            string usuario = "testuser";
+            string accion = "Test action";
+            string rutaArchivo = usuario + ".log";
+
+            Log.escribirLog(usuario, accion);
+
+            string[] lineas = File.ReadAllLines(rutaArchivo);
+            Assert.AreEqual(1, lineas.Length);
+            StringAssert.Contains(lineas[0], "Fecha:");
+            StringAssert.Contains(lineas[0], "Accion: " + accion);
+
+            File.Delete(rutaArchivo);
+        }
+    }
+}
---
+++ ClassLibTests/UsuarioTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using ClassLib;
+
+namespace ClassLibTests
+{
+    [TestClass]
+    public class UsuarioTests
+    {
+        [TestMethod]
+        public void CambiarEmail_CambiaElEmailCorrectamente()
+        {
+            string emailInicial = "usuario@ejemplo.com";
+            string emailNuevo = "nuevo@ejemplo.com";
+            Usuario usuario = new Usuario(emailInicial, "Contrasena123");
+
+            usuario.CambiarEmail(emailNuevo);
+
+            Assert.AreEqual(emailNuevo, usuario.getEmailUsuario());
+
+            File.Delete(emailInicial + ".log");
+            File.Delete(emailNuevo + ".log");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "El email no puede ser nulo o igual al anterior")]
+        public void CambiarEmail_LanzaExcepcionSiEmailEsNulo()
+        {
+            string emailInicial = "usuario@ejemplo.com";
+            Usuario usuario = new Usuario(emailInicial, "Contrasena123");
+
+            usuario.CambiarEmail(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "El email no puede ser nulo o igual al anterior")]
+        public void CambiarEmail_LanzaExcepcionSiEmailEsIgualAlAnterior()
+        {
+            string emailInicial = "usuario@ejemplo.com";
+            Usuario usuario = new Usuario(emailInicial, "Contrasena123");
+
+            usuario.CambiarEmail(emailInicial);
+        }
+
+        [TestMethod]
+        public void CambiarContrasena_CambiaLaContrasenaCorrectamente()
+        {
+            string email = "usuario@ejemplo.com";
+            string contrasenaInicial = "Contrasena123";
+            string contrasenaNueva = "NuevaContrasena123";
+            Usuario usuario = new Usuario(email, contrasenaInicial);
+
+            usuario.cambiarContrasena(contrasenaNueva);
+
+            // PREGUNTAR COMO HACERLO
+            // No hay un método público para obtener la contraseña, así que verificamos que no lanza excepción
+            Assert.IsTrue(true);
+
+            File.Delete(email + ".log");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "La contrasena no cumple con los requisitos minimos")]
+        public void CambiarContrasena_LanzaExcepcionSiContrasenaNoCumpleRequisitos()
+        {
+            string email = "usuario@ejemplo.com";
+            string contrasenaInicial = "Contrasena123";
+            string contrasenaNueva = "123";
+            Usuario usuario = new Usuario(email, contrasenaInicial);
+
+            usuario.cambiarContrasena(contrasenaNueva);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "La contrasena no puede ser igual a una de las 10 anteriores")]
+        public void CambiarContrasena_LanzaExcepcionSiContrasenaEsIgualAUnaAnterior()
+        {
+            string email = "usuario@ejemplo.com";
+            string contrasenaInicial = "Contrasena123";
+            Usuario usuario = new Usuario(email, contrasenaInicial);
+
+            usuario.cambiarContrasena(contrasenaInicial);
+        }
+    }
+}
