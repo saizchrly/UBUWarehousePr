9859bf40c36b158751fa3650cf459cf0eda932d5
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-10-09 21:39:24 +0200

    #4 Agregamos la data base

---
+++ WHDatabase/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("WHDatabase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WHDatabase")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("b1852d5c-6ed6-4710-9d18-195bbeeca1d0")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WHDatabase/WHdb.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLib;
+
+namespace WHDatabase
+{
+    public class WHdb : IWHdb
+    {
+        Dictionary<int, Usuario> tblUsuarios = new Dictionary<int, Usuario>();
+        int proximoIDUsuario = 1;
+
+        public WHdb()
+        {
+            Usuario u = new Usuario("prueba1@ubu.es", "prueba1", "Admin")
+        }
+        public bool GuardaComponente(Elemento e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GuardaUsuario(Usuario u)
+        {
+            if (u.getIdUsuario() < 0)
+            {
+                u.setIdUsuario(proximoIDUsuario);
+                proximoIDUsuario++;
+            }
+            tblUsuarios[u.getIdUsuario()] = u;
+            return true;
+        }
+
+        public Elemento LeeComponente(int idElemento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario? LeeUsuario(string email)
+        {
+            foreach (Usuario utemp in tblUsuarios.Values)
+            {
+                if (utemp.getEmailUsuario().Equals(email)) return utemp;
+            }
+            return null;
+        }
+
+        public int NumComponente()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NumUsuarios()
+        {
+            return tblUsuarios.Count;
+        }
+
+        public int NumUsuariosActivos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidaUsuario(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
