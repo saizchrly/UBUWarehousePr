d30356c7d861aa5358286b6023f2271a17b5e892
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-10-18 15:03:48 +0200

    #7 cambio log

--- ClassLib/Log.cs
+++ ClassLib/Log.cs
@@ -12,11 +12,10 @@ namespace ClassLib
         private const string path = @"..\..\..\..\UBUWarehousePr\ClassLib\Logs\";
 
         public static void escribirLog(string usuario, string Accion)
-        {
-            string usuarioSinTLD = usuario.Substring(0, usuario.LastIndexOf('.'));
+        {            
             DateTime fecha = DateTime.Now;
             string texto =" Fecha: " + fecha + " Accion: " + Accion;
-            Utilidades.EscribirEnArchivo(path + usuarioSinTLD + ".log", texto);
+            Utilidades.EscribirEnArchivo(path + usuario + ".log", texto);
         } 
     }
 }
